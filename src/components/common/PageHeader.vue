07ee37dc25dbbf23b9e6eb107cce872de1657887
Milena Valcheva <milena_77@mail.bg>
2019-08-16 11:18:29 +0300

    change file

--- src/components/common/PageHeader.vue
+++ src/components/common/PageHeader.vue
@@ -1,30 +1,44 @@
 <template>
-    <mdb-navbar color="indigo" class="mt-2" expand="xl" dark animated animation="2">
-      <!-- Navbar brand -->
-      <mdb-navbar-brand class="text-white">Navbar</mdb-navbar-brand>
-      <mdb-navbar-toggler>
-        <mdb-navbar-nav right>
-          <mdb-nav-item href="#" waves-fixed>Home</mdb-nav-item>
-          <mdb-nav-item href="#" waves-fixed>Features</mdb-nav-item>
-          <mdb-nav-item href="#" waves-fixed>Pricing</mdb-nav-item>
-        </mdb-navbar-nav>
-      </mdb-navbar-toggler>
-    </mdb-navbar>
+  <mdb-navbar expand="large" dark color="indigo" class="mt-2" animated animation="2">
+    <mdb-navbar-brand href="/">
+      Tasty
+    </mdb-navbar-brand>
+    <mdb-navbar-toggler>
+      <mdb-navbar-nav right>
+        <mdb-nav-item href="/" active><i class="fas fa-home"></i> Home</mdb-nav-item>
+        <mdb-nav-item href="#"><i class="fas fa-plus-circle"></i> Create</mdb-nav-item>
+        <mdb-nav-item href="#"><i class="fas fa-smile"></i> Profile</mdb-nav-item>
+        <mdb-nav-item href="/register"><i class="fas fa-user"></i> Register</mdb-nav-item>
+        <mdb-nav-item href="/login"><i class="fas fa-user"></i> Login</mdb-nav-item>
+        <mdb-nav-item class="disabled" href="#">Logout</mdb-nav-item>
+        <!-- <mdb-dropdown tag="li" class="nav-item">
+          <mdb-dropdown-toggle tag="a" navLink color="indigo" slot="toggle" waves-fixed>Dropdown</mdb-dropdown-toggle>
+          <mdb-dropdown-menu>
+            <mdb-dropdown-item>Action</mdb-dropdown-item>
+            <mdb-dropdown-item>Another action</mdb-dropdown-item>
+            <mdb-dropdown-item>Something else here</mdb-dropdown-item>
+          </mdb-dropdown-menu>
+        </mdb-dropdown> -->
+      </mdb-navbar-nav>
+    </mdb-navbar-toggler>
+  </mdb-navbar>
 </template>    
 
 <script>
-import { mdbNavbar, mdbNavItem, mdbNavbarNav, mdbNavbarToggler, mdbContainer, mdbRow, mdbInput, mdbNavbarBrand, mdbIcon } from 'mdbvue';
+import { mdbDropdown, mdbDropdownToggle, mdbDropdownMenu, mdbDropdownItem, mdbContainer, mdbNavbar, mdbNavbarBrand, mdbNavbarToggler, mdbNavbarNav, mdbNavItem, mdbIcon } from 'mdbvue';
 
 export default {
     components: {
       mdbNavbar,
-      mdbNavItem,
-      mdbNavbarNav,
-      mdbNavbarToggler,
-      mdbContainer,
-      mdbRow,
-      mdbInput,
       mdbNavbarBrand,
+      mdbNavbarToggler,
+      mdbNavbarNav,
+      mdbNavItem,
+      mdbContainer,
+      mdbDropdown,
+      mdbDropdownToggle,
+      mdbDropdownMenu,
+      mdbDropdownItem,
       mdbIcon
     }   
 }
